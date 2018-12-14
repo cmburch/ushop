c6c61f2e47e3e025c2a90be919615b568fe22183
cmburch <georgia_beast89@yahoo.com>
2018-12-14 16:57:12 +0000

    add home and shopping-cart component

--- ushop/src/app/app.module.ts
+++ ushop/src/app/app.module.ts
@@ -9,11 +9,15 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
