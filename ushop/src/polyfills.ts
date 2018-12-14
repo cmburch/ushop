5b9facec1a2152861ab1801cbe46d6e67ffb985d
cmburch <georgia_beast89@yahoo.com>
2018-12-14 16:04:30 +0000

    fixed dev env build error this is a temp work around

--- ushop/src/polyfills.ts
+++ ushop/src/polyfills.ts
@@ -83,6 +83,8 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+(window as any).global = window;
+
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
