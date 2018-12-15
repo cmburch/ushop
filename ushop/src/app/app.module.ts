a780cd98c417405c663ad63f64d13abad30138cd
cmburch <georgia_beast89@yahoo.com>
2018-12-15 01:19:08 +0000

    added ng-bootstrap and created ngbootrap dropdown menu

--- ushop/src/app/app.module.ts
+++ ushop/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { HomeComponent } from './home/home.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './check-out/check-out.component';
@@ -16,7 +18,6 @@ import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-
 import { LoginComponent } from './login/login.component';
 import { ProductsComponent } from './products/products.component';
 
@@ -41,6 +42,7 @@ import { ProductsComponent } from './products/products.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
+    NgbModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'login', component: LoginComponent },
